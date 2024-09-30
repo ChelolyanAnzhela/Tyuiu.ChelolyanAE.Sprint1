1f67dfed481994c74109763c433ad947c616bacd
ChelolyanAnzhela <Tyuiu.Chelolyanae@outlook.com>
2024-09-30 19:32:11 +0500

    Task2

---
+++ Tyuiu.ChelolyanAE.Sprint1.Task2.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.ChelolyanAE.Sprint1.Task2.V29.Lib;
+
+namespace Tyuiu.ChelolyanAE.Sprint1.Task2.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 |Выполнила: Челольян А. Е. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Арифметические операторы в C#                                     *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнила: Челольян Анжела Евгеньевна | ИИПБ-24-2                       *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране              *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int x;
+            Console.WriteLine("* Введите значение X:");
+            x=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Время в минутах x=" + ds.ConvertSecondsToHours(x));
+            Console.ReadLine();
+        }
+    }
+}
